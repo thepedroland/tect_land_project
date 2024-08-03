9e81ff0878e0a4d399be19b43260502021951310
pedrosilvestre442@gmail.com <115794613+s-oliveira-pedro@users.noreply.github.com>
2024-08-02 21:00:27 -0600

    added strike function

--- FieldWork_class.py
+++ FieldWork_class.py
@@ -9,8 +9,8 @@ class FieldWork:
         dip_mean = np.mean(self.data["dip"])
         strike_mean = np.mean(self.data["strike"])
 
-        print(f"The average strike is {strike_mean}")
-        print(f"The average dip is {dip_mean}")
+        print(f"The average strike is {strike_mean} degrees")
+        print(f"The average dip is {dip_mean} degrees")
         
         return dip_mean
 
@@ -26,8 +26,29 @@ class FieldWork:
         print(f"Your area has {self.x_size} km x {self.y_size} km, the grid has {self.rows} rows and {self.col} columns")
         
         return self.rows, self.col
-
     
+    def calculate_top_xy_coordinate(self, grid,x_bottom, y_bottom, angle_degrees):
+        
+        self.grid_size = (grid.shape[1], grid.shape[0])  # (x_max, y_max)
+        angle_radians = np.radians(angle_degrees)
+        x_max, y_max = self.grid_size
+
+        L_top = y_max / np.sin(angle_radians)
+        L_right = (x_max - x_bottom) / np.cos(angle_radians)
+
+        L = min(L_top, L_right)
+
+        delta_x = L * np.cos(angle_radians)
+        delta_y = L * np.sin(angle_radians)
+
+        x_top = x_bottom + delta_x
+        y_top = y_bottom + delta_y
+
+        x_top = min(x_top, x_max)
+        y_top = min(y_top, y_max)
+
+        return (x_top, y_top)
+
 
 
 
